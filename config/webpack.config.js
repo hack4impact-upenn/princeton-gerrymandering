17e2c87da7269f95316b2fbc493d510b9fd20378
sarahraines <saraines@seas.upenn.edu>
2020-04-13 17:49:09 -0400

    fix webpack svg config

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -78,7 +78,7 @@ module.exports = {
                 use: ['@svgr/webpack'],
             },
             {
-                test: /\.(eot|woff|woff2|ttf)$/,
+                test: /\.(eot|woff|woff2|ttf|svg|png|jpg)$/,
                 loader: 'url-loader?limit=30000&name=[name]-[hash].[ext]'
             }
         ]
