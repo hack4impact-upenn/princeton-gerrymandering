378e8420b86fffd520f059dc01b1595a5911de74
Ben Demers <ben.n.demers@gmail.com>
2020-08-25 20:27:09 -0400

    api route for suggestions

--- api/blueprints/api.py
+++ api/blueprints/api.py
@@ -11,25 +11,7 @@ from elasticsearch import Elasticsearch, RequestsHttpConnection
 import boto3
 import boto3.session
 
-import tensorflow as tf
-import tensorflow_hub as hub
-import tf_sentencepiece
-import time
-import numpy as np
-import faiss
-from annoy import AnnoyIndex
-from elasticsearch import Elasticsearch
-from elasticsearch_dsl import Search
-import numpy as np
-import unicodedata
-import csv 
-from os.path import basename
-import unicodedata
-from .to_sentences import *
-from requests_aws4auth import AWS4Auth
-import sys 
-from .search import *
-
+import requests
 
 from util.elasticsearch_queries import (
     or_contains_filter, or_not_contains_filter,
@@ -43,9 +25,6 @@ from flask_jwt_extended import (
     get_jwt_identity, set_access_cookies,
     set_refresh_cookies, unset_jwt_cookies
 )
-
-def generate_embeddings (messages_in):
-    return session.run(embedded_text, feed_dict={text_input: messages_in})
 
 with open(os.path.join(os.path.dirname(__file__), "..", "config", "config.json")) as f:
     config = json.load(f)
@@ -74,38 +53,6 @@ with open(os.path.join(os.path.dirname(__file__), "..", "config", "config.json")
     s3_client = session.client(
         's3', config=boto3.session.Config(signature_version='s3v4'))
         
-    use_module_url = "https://tfhub.dev/google/universal-sentence-encoder-multilingual/1"
-    g = tf.Graph()
-    with g.as_default():
-        text_input = tf.placeholder(dtype=tf.string, shape=[None])
-        embed_module = hub.Module(use_module_url)
-        embedded_text = embed_module(text_input)
-        init_op = tf.group([tf.global_variables_initializer(), tf.tables_initializer()])
-    g.finalize()
-
-    session = tf.Session(graph=g)
-    session.run(init_op)
-
-    ES_INDEX_FULL_TEXT = config.get("ES_INDEX_FULL_TEXT")
-    ES_INDEX_CHUNK = config.get("ES_INDEX_CHUNK")
-    vector_dims = 512
-    vector_index = AnnoyIndex(vector_dims, 'angular')
-    annoy_fn = config.get("ANNOY_FN")
-    print(annoy_fn)
-    vector_index.load(annoy_fn) # super fast, will just mmap the file
-
-
-    with open(config.get("IDX_NAME"), 'r') as f:
-        idx_name = json.load(f)
-    with open(config.get("NAME_IDX"), 'r') as f:
-        name_idx = json.load(f)
-    vec_cnt = vector_index.get_n_items()
-
-    searcher = QzUSESearchFactory(vector_index, idx_name, name_idx, es, ES_INDEX_FULL_TEXT, ES_INDEX_CHUNK, generate_embeddings)
-
-
-
-
 
 def configure_api(app):
     api = Blueprint('api', __name__)
@@ -174,18 +121,10 @@ def configure_api(app):
     @api.route("/suggest/<string:id>", methods=["GET"])
     @jwt_required
     def suggest(id):
-        try:
-            searcher = searcher.query_by_doc_text(id, k=50)
-            recomendations = searcher.show(show_seed_docs=False)
-        except:
-            recomendations = []
-        for rec in recomendations:
-            recData = resource(rec)
-            if(recData):
-                recs_results.append(json.dumps(recData))      
-        return jsonify({
-            'recs' : recs_results
-            }), 200
+        x = requests.post("http://127.0.0.1:5000/predict", params = {"id":"test"})
+        return x
+
+        
     
     @api.route("/tags/add", methods=["POST"])
     @jwt_required
